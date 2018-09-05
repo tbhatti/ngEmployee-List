09cb731a97a813d532000b34cefcdfe83bae4f55
Tanzeem Bhatti <Tanzeem_Bhatti@epam.com>
2018-09-05 15:17:28 +0800

    Angular 6 with bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs'
+
+var EmployeesList = require('./employees.json');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employees-list';
+  employees = EmployeesList;
+  employeeData = {};
+  constructor(private modal: NgbModal) { }
+
+  toggleEmployee(employee) {
+    this.employees = this.employees.map((emp) => {
+      if (employee.id === emp.id) {
+        employee.opened = !employee.opened
+        return employee
+      } else {
+        emp.opened = false;
+        return emp;
+      }
+    })
+
+  }
+
+  showEmployeeDetails(modalContent, employee) {
+    console.log('EMPLOYEE', employee)
+    this.employeeData = {
+      employee
+    };
+    this
+      .modal
+      .open(modalContent, { size: 'lg' })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
